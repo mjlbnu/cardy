00ffdf1bc91c806cd9ecd706b1a02898dbf3ff4d
Marcio Lisboa <mjlbnu@gmail.com>
2021-05-12 23:19:01 -0300

    Feat(Home) Adding first static components

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,9 +1,31 @@
+import { Container } from 'components/Container'
+import Footer from 'components/Footer'
+import Heading from 'components/Heading'
+import Menu from 'components/Menu'
 import * as S from './styles'
 
 const Home = () => (
-  <S.Wrapper>
-    <h1>Home</h1>
-  </S.Wrapper>
+  <section>
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        Why Cardy
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Who’s Using Cardy
+      </Heading>
+    </Container>
+
+    <Container>
+      <Footer />
+    </Container>
+  </section>
 )
 
 export default Home
